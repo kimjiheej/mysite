3706c8443016e364d3dc10283d3ae65926567412
kimjiheej <glp0509@naver.com>
2024-06-17 10:48:33 +0900

    JsonResult

--- mysite3/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -11,10 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.poscodx.mysite.dto.JsonResult;
 import com.poscodx.mysite.security.Auth;
 import com.poscodx.mysite.security.AuthUser;
 import com.poscodx.mysite.service.UserService;
@@ -29,6 +31,15 @@ public class UserController {
 	@RequestMapping(value="/join", method=RequestMethod.GET)
 	public String join(@ModelAttribute UserVo vo) {
 		return "user/join";
+	}
+	
+	@GetMapping("/checkemail") 
+	public JsonResult checkEmail(@RequestParam(value="email", required = true)) {
+		UserVo vo = userService.getUser(email);
+		JsonResult jsonResult = new JsonResult();
+		
+		jsonResult.setResult("ok");
+		return jsonResult;
 	}
 	
 	@RequestMapping(value="/join", method=RequestMethod.POST)
