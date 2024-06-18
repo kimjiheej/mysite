507735aa374cae4a8c2f184add90b4baa39028b8
kimjiheej <glp0509@naver.com>
2024-06-18 17:12:45 +0900

    Java Config 로 변경 완료

--- mysite4/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite4/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -95,4 +95,13 @@ public class UserController {
 		return "redirect:/user/update";
 	}
 	
+	@RequestMapping("/auth")
+	public void auth() {
+	}
+
+	@RequestMapping("/logout")
+	public void logout() {
+	}
+	
+	
 }
--- mysite4/src/main/java/com/poscodx/mysite/security/LoginInterceptor.java
+++ mysite4/src/main/java/com/poscodx/mysite/security/LoginInterceptor.java
@@ -11,39 +11,32 @@ import com.poscodx.mysite.service.UserService;
 import com.poscodx.mysite.vo.UserVo;
 
 public class LoginInterceptor implements HandlerInterceptor {
-
 	@Autowired
 	private UserService userService;
-
-	public LoginInterceptor(UserService userService) {
-
-		this.userService = userService;
-	}
-
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
-		UserVo authUser = 	userService.getUser(email,password);
 
-		if(authUser == null ) {
+		UserVo authUser = userService.getUser(email, password);
+		if(authUser == null) {
 			request.setAttribute("email", email);
 			request.setAttribute("result", "fail");
-			request.getRequestDispatcher("/WEB-INF/views/user/login.jsp")
-			.forward(request, response);
+			request
+				.getRequestDispatcher("/WEB-INF/views/user/login.jsp")
+				.forward(request, response);
 			return false;
 		}
-
-		/* 로그인 처리 */ 
 		
+		/* login 처리 */
 		System.out.println(authUser);
-	    HttpSession session = request.getSession(true);
-	    session.setAttribute("authUser", authUser);
-	    response.sendRedirect(request.getContextPath());
-	    return false;
+		
+		HttpSession session = request.getSession(true);
+		session.setAttribute("authUser", authUser);
+		response.sendRedirect(request.getContextPath());
+
+		return false;
 	}
-
-
 }
